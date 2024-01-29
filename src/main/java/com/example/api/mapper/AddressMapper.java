9aa953b329db92809d79479cd9f6b18c9cf77a8e
Evaldo <ecruz.mazzatech@ext.valid.com>
2024-01-29 14:26:07 -0300

    Ajustes no mapstruct

--- src/main/java/com/example/api/mapper/AddressMapper.java
+++ src/main/java/com/example/api/mapper/AddressMapper.java
@@ -4,12 +4,14 @@ import com.example.api.domain.Address;
 import com.example.api.domain.dto.AddressRequest;
 import com.example.api.domain.dto.AddressResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface AddressMapper {
 
+    @Mapping(target = "id", ignore = true)
     Address requestToEntity(AddressRequest request);
 
     AddressResponse entityToResponse(Address entity);
--- src/main/java/com/example/api/mapper/CustomerMapper.java
+++ src/main/java/com/example/api/mapper/CustomerMapper.java
@@ -4,12 +4,15 @@ import com.example.api.domain.Customer;
 import com.example.api.domain.dto.CustomerRequest;
 import com.example.api.domain.dto.CustomerResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "addresses", ignore = true)
     Customer requestToEntity(CustomerRequest request);
 
     CustomerResponse entityToResponse(Customer entity);
